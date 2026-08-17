dfd40442fc42a268a671cccb352f634e750c2e73
Dagim Gedle <Gelda1214@gmail.com>
2026-08-17 16:30:56 -0700

    Sneaker App

---
+++ Kind1.py
@@ -0,0 +1,10 @@
+from main import Sneakers
+
+
+class Basketball (Sneakers):
+    def __init__(self, name, price, size, brand, bounce):
+        self.name = name 
+        self.price = price
+        self.size = size
+        self.brand = brand
+        self.bounce = bounce 
---
+++ Kind2.py
@@ -0,0 +1,9 @@
+from main import Sneakers
+
+class RunningShoes(Sneakers):
+    def __init__(self, name, price, size, color, comfort,):
+        self.name = name
+        self.price = price
+        self.size = size
+        self.color = color
+        self.comfort = comfort
